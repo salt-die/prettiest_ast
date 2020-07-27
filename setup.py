a0f1d77bacb1b48249e6c1c3d860186179a177d6
salt-die <naclmolecule@gmail.com>
2020-07-27 03:00:26 -0500

    add description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+# Description from README.md
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='prettiest_ast',
       version='0.1',
       url='https://github.com/salt-die/prettiest_ast',
       author='salt-die',
       author_email='saltdie.py@gmail.com',
       license='MIT',
-      packages=['prettiest_ast'])
+      packages=['prettiest_ast'],
+      long_description=long_description,
+      long_description_content_type='text/markdown')
