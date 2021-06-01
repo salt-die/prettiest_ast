59ad9e581e5c03d0e425097d9660499bee897821
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-31 23:08:14 -0500

    Functions renamed.

--- prettiest_ast/__init__.py
+++ prettiest_ast/__init__.py
@@ -1,3 +1,3 @@
-"""prettiest_ast is a pretty printer for abstract syntax trees."""
-
-from .prettiest_ast import formatast, ppast
+"""A pretty printer for abstract syntax trees.
+"""
+from .prettiest_ast import format_ast, pp_ast
